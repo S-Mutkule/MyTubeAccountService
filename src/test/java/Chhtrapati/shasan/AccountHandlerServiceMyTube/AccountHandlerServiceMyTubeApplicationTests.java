2afba56e668e0fb67c52a1f07797695c1b3452df
Sarwadnya Mutkule <sarwadnyamutkule99@gmail.com>
2025-05-21 01:03:19 +0530

    Committing all files version 1

---
+++ src/test/java/Chhtrapati/shasan/AccountHandlerServiceMyTube/AccountHandlerServiceMyTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package Chhtrapati.shasan.AccountHandlerServiceMyTube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountHandlerServiceMyTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
